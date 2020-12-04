3944534347079b58c421d7c4a5d5d1fccfbf59a9
Sue Bluck <SBluck@academytrainee.com>
2020-12-04 14:44:49 +0000

    SB: amend get js to include by status and remove xs fields

--- js/get.js
+++ js/get.js
@@ -12,19 +12,35 @@ function getPet(petId) {
                 // Examine the text in the response
                 response.json().then(function (data) {
                     console.log(data);
-                    console.log("catId", data.category.id);
-                    console.log("catName", data.category.name);
                     console.log("name", data.name);
-//                    for
-                    console.log("photoUrl", data.photoUrls[0]);
-//                    for
-//                      for
-                    console.log("photoTagId", data.tags[0]);
-//                    console.log("photoTagName", data.tags[0].tag.name);
-
                     console.log("status", data.status);
                 });
             })
             .catch(err => console.error(err));
 }
-getPet(5);
+function getPetByStatus(status) {
+
+    fetch(`https://petstore.swagger.io/v2/pet/findByStatus?status=${status}`)  
+        .then(
+            function (response) {
+                if (response.status !== 200) {
+                    console.log('Looks like there was a problem. Status Code: ' +
+                        response.status);
+                    return;
+                }
+
+                // Examine the text in the response
+                response.json().then(function (data) {
+                    console.log(data);
+
+                    for(let i=0;i<data.length;i++) {
+
+                        console.log("catId", data[i].id);
+                        console.log("name", data[i].name);
+                        console.log("status", data[i].status);
+                    }
+                });
+            })
+            .catch(err => console.error(err));
+}
+getPetByStatus("sold");
