cc364469ab6a9d601ffd81502f79844955de08d0
Fayaz(Work) <gxz>
2020-12-04 12:34:44 +0000

    Delete Pet Connected

--- js/delete.js
+++ js/delete.js
@@ -1,4 +1,5 @@
-function deletePet(petID) {
+function deletePet() {
+    const petID = document.getElementById("delPetID").value;
     fetch(`https://petstore.swagger.io/v2/pet/${petID}`, {method: 'delete'})
         .then(response => response.json())
         .then(json => {return json;})
